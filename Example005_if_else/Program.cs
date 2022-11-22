a7e28c5eda773524f1882c60278e1f00cf9003bf
Natasha <n.chalganova@mail.ru>
2022-11-22 15:13:40 +0300

    if_else

---
+++ Example005_if_else/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "наташа")
+{
+    Console.WriteLine ("Здравствуй дорогая, Наташа");
+}
+ 
+ else 
+{
+    Console.Write("Привет, ");
+    Console.WriteLine (username);
+}
